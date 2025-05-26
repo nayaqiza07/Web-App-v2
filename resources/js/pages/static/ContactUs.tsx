8364c10a3e98e2e0ad4c822cde1bda0dacd07c13
nayaqiza07 <muhammadnaza07@gmail.com>
2025-05-26 09:15:17 +0700

    APP-12 add contact & about us page ✨

---
+++ resources/js/pages/static/ContactUs.tsx
@@ -0,0 +1,12 @@
+import { Head } from '@inertiajs/react';
+
+const ContactUs = () => {
+    return (
+        <>
+            <Head title="Contact Us" />
+            <h1>Contact Us</h1>
+        </>
+    );
+};
+
+export default ContactUs;
