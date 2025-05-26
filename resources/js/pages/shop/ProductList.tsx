89abd35548f1db70bbfdd475bd19f1565173febf
nayaqiza07 <muhammadnaza07@gmail.com>
2025-05-26 08:52:32 +0700

    APP-8 add shop page ✨

---
+++ resources/js/pages/shop/ProductList.tsx
@@ -0,0 +1,12 @@
+import { Head } from '@inertiajs/react';
+
+const ProductList = () => {
+    return (
+        <>
+            <Head title="Product List" />
+            <h1>Product List</h1>
+        </>
+    );
+};
+
+export default ProductList;
