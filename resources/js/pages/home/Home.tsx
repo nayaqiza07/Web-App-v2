fb747198d43b9ad67533b3dbe92b1cf8efe3b763
nayaqiza07 <muhammadnaza07@gmail.com>
2025-05-26 08:41:31 +0700

    APP-8 add homepage ✨

---
+++ resources/js/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import { Head } from '@inertiajs/react';
+
+const Home = () => {
+    return (
+        <>
+            <Head title="Home" />
+            <h1>Home</h1>
+        </>
+    );
+};
+
+export default Home;
