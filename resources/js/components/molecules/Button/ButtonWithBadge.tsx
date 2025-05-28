3be102a64254a2dc076367eb0de75bfb32f7989a
nayaqiza07 <muhammadnaza07@gmail.com>
2025-05-28 14:26:32 +0700

    APP-30 add sidebar missing file ✨

---
+++ resources/js/components/molecules/Button/ButtonWithBadge.tsx
@@ -0,0 +1,20 @@
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+
+interface ButtonWithBadgeProps {
+    title: string;
+    badgeNumber: number;
+}
+
+const ButtonWithBadge = ({ title = 'Title', badgeNumber = 0 }: ButtonWithBadgeProps) => {
+    return (
+        <Button variant="ghost" className="flex justify-between p-2 text-sm font-bold">
+            <span>{title}</span>
+            <Badge variant="outline" className="bg-card border">
+                {badgeNumber}
+            </Badge>
+        </Button>
+    );
+};
+
+export default ButtonWithBadge;
