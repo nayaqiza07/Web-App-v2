90caa7a4c6bcf4acc412b09c4850ad71a012a0de
nayaqiza07 <muhammadnaza07@gmail.com>
2025-06-30 08:31:08 +0700

    APP-57 fixing dummy data for products

--- resources/js/components/templates/Home/Homepage.tsx
+++ resources/js/components/templates/Home/Homepage.tsx
@@ -1,6 +1,5 @@
 import CallToAction from '@/components/molecules/CTA/CallToAction';
 import Activity from '@/components/organisms/Activity/Activity';
-import CarouselProduct from '@/components/organisms/Carousel/CarouselProduct';
 import HeroSection from '@/components/organisms/Section/HeroSection';
 import { Button } from '@/components/ui/button';
 import { Link } from '@inertiajs/react';
@@ -23,7 +22,7 @@ const Homepage = () => {
             <HeroSection isLoading={isLoading} srcImage="/images/image-18.jpg" altImage="Image Slider" className="gap-10">
                 <h1 className="text-2xl font-bold md:text-4xl">New Styles Are Here</h1>
                 <p className="text-xs md:text-base">Discover The Lasts Premium FurnitureFeatured In Our Collection</p>
-                <Link href={route('products')}>
+                <Link href={route('products.index')}>
                     <Button variant="secondary" className="rounded-full text-xs font-bold">
                         Go Shopping
                     </Button>
@@ -31,10 +30,10 @@ const Homepage = () => {
             </HeroSection>
 
             {/* 2 */}
-            <CarouselProduct isLoading={isLoading} headLineTitle="What's New" isAutoPlay totalItemShow="basis-1/2 md:basis-1/4 lg:basis-1/5" />
+            {/* <CarouselProduct isLoading={isLoading} headLineTitle="What's New" isAutoPlay totalItemShow="basis-1/2 md:basis-1/4 lg:basis-1/5" /> */}
 
             {/* 3 */}
-            <CarouselProduct isLoading={isLoading} headLineTitle="Categories" isFor="category" isAutoPlay totalItemShow="md:basis-1/2" />
+            {/* <CarouselProduct isLoading={isLoading} headLineTitle="Categories" isFor="category" isAutoPlay totalItemShow="md:basis-1/2" /> */}
 
             {/* 4 */}
             <CallToAction />
@@ -58,7 +57,7 @@ const Homepage = () => {
             />
 
             {/* 6 */}
-            <CarouselProduct isLoading={isLoading} headLineTitle="Recent Blogs" isFor="blog" isAutoPlay totalItemShow="md:basis-1/4 lg:basis-1/5" />
+            {/* <CarouselProduct isLoading={isLoading} headLineTitle="Recent Blogs" isFor="blog" isAutoPlay totalItemShow="md:basis-1/4 lg:basis-1/5" /> */}
         </>
     );
 };
