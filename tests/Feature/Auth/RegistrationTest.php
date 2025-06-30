f8effa4f477a2e93fc4bc726e44d632599b2dcb4
nayaqiza07 <muhammadnaza07@gmail.com>
2025-06-30 11:25:00 +0700

    APP-58 fixing registration user test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -10,6 +10,7 @@ test('new users can register', function () {
     $response = $this->post('/register', [
         'name' => 'Test User',
         'email' => 'test@example.com',
+        'phone' => '08123456789',
         'password' => 'password',
         'password_confirmation' => 'password',
     ]);
